cbc19fc783a946474bd7ac81ad104952a3aa6c01
justYu2001 <jack4781012@gmail.com>
2022-03-11 06:57:52 -0800

    implement gpio command in lab2-1

--- lab2/compil.sh
+++
@@ -1,1 +0,0 @@
-aarch64-linux-gnu-g++ -o L2Program lab2_1.cpp
--- lab2/lab2_1.cpp
+++ lab2/lab2_1.cpp
@@ -1,12 +1,64 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <fstream>
+#include <string>
+#include <cerrno>
 
 using namespace std;
+
+void setExport(unsigned int gpio) {
+	ofstream outFileStream;
+
+	outFileStream.open("/sys/class/gpio/export");
+
+	if(!outFileStream.is_open()) {
+		perror("failed to open export");
+		return;
+	} else {
+		outFileStream << gpio;
+		outFileStream.close();
+	}
+}
+
+void setUnexport(unsigned int gpio) {
+	ofstream outFileStream;
+
+	outFileStream.open("/sys/class/gpio/unexport");
+
+	if(!outFileStream.is_open()) {
+		perror("failed to open unexport");
+		return;
+	} else {
+		outFileStream << gpio;
+		outFileStream.close();
+	}
+}
+
+void setDirection(unsigned int gpio, string direction) {
+	ofstream outFileStream;
+
+	outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/direction");
+
+	if(!outFileStream.is_open()) {
+		perror("failed to open direction");
+		return;
+	} else {
+		outFileStream << direction;
+		outFileStream.close();
+	}
+}
+
+void setValue(unsigned int gpio, int value) {
+	ofstream outFileStream;
+
+	outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/value");
+
+	if(!outFileStream.is_open()) {
+		perror("failed to open value");
+		return;
+	} else {
+		outFileStream << value;
+		outFileStream.close();
+	}
+}
 
 int main(int argc, char *argv[]) {
     int leds[4] = {396, 466, 392, 481};
@@ -16,16 +68,16 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < 4; i++) {
         if (led[3] - '0' == i) {
-	    if (value[1] == 'n') {
-	        gpio_export(leds[i]);
-		gpio_set_dir(leds[i], "out");
-		gpio_set_value(leds[i], 1);
-	    } else {
-	        gpio_set_value(leds[i], 0);
-		gpio_unexport(leds[i]);
-	    }
-	    break;
-	}
+	    	if (value[1] == 'n') {
+				setExport(leds[i]);
+				setDirection(leds[i], "out");
+				setValue(leds[i], 1);
+			} else {
+				setValue(leds[i], 0);
+				setUnexport(leds[i]);
+			}
+			break;
+		}
     }
 
     return 0;
