e16db140d080088719e6eb2b6888c5a548c07d85
justYu2001 <jack4781012@gmail.com>
2022-03-18 00:43:58 -0700

    fix lab1 error

--- lab2/gpio.cpp
+++ lab2/gpio.cpp
@@ -1,62 +1,63 @@
 #include <fstream>
 #include <string>
 #include <cerrno>
+#include <iostream>
 #include "gpio.h"
 
 using namespace std;
 
 void setExport(unsigned int gpio) {
-	ofstream outFileStream;
+    ofstream outFileStream;
 
-	outFileStream.open("/sys/class/gpio/export");
+    outFileStream.open("/sys/class/gpio/export");
 
-	if(!outFileStream.is_open()) {
-		perror("failed to open export");
-		return;
-	} else {
-		outFileStream << gpio;
-		outFileStream.close();
-	}
+    if(!outFileStream.is_open()) {
+        perror("failed to open export");
+        return;
+    } else {
+        outFileStream << gpio;
+        outFileStream.close();
+    }
 }
 
 void setUnexport(unsigned int gpio) {
-	ofstream outFileStream;
+    ofstream outFileStream;
 
-	outFileStream.open("/sys/class/gpio/unexport");
+    outFileStream.open("/sys/class/gpio/unexport");
 
-	if(!outFileStream.is_open()) {
-		perror("failed to open unexport");
-		return;
-	} else {
-		outFileStream << gpio;
-		outFileStream.close();
-	}
+    if(!outFileStream.is_open()) {
+        perror("failed to open unexport");
+        return;
+    } else {
+        outFileStream << gpio;
+        outFileStream.close();
+    }
 }
 
 void setDirection(unsigned int gpio, string direction) {
-	ofstream outFileStream;
+    ofstream outFileStream;
 
-	outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/direction");
+    outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/direction");
 
-	if(!outFileStream.is_open()) {
-		perror("failed to open direction");
-		return;
-	} else {
-		outFileStream << direction;
-		outFileStream.close();
-	}
+    if(!outFileStream.is_open()) {
+        perror("failed to open direction");
+        return;
+    } else {
+        outFileStream << direction;
+        outFileStream.close();
+    }
 }
 
 void setValue(unsigned int gpio, int value) {
-	ofstream outFileStream;
+    ofstream outFileStream;
 
-	outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/value");
+    outFileStream.open("/sys/class/gpio/gpio" + to_string(gpio) + "/value");
 
-	if(!outFileStream.is_open()) {
-		perror("failed to open value");
-		return;
-	} else {
-		outFileStream << value;
-		outFileStream.close();
-	}
+    if(!outFileStream.is_open()) {
+        perror("failed to open value");
+        return;
+    } else {
+        outFileStream << value;
+        outFileStream.close();
+    }
 }
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,64 +2,73 @@
 #include <cerrno>
 #include <chrono>
 #include <thread>
+#include <iostream>
 #include "gpio.h"
 
 using namespace std;
 
 void turnOnLED(unsigned int gpio) {
-	setExport(gpio);
-	setDirection(gpio, "out");
-	setValue(gpio, 1);
+    setExport(gpio);
+    setDirection(gpio, "out");
+    setValue(gpio, 1);
+    setUnexport(gpio);
 }
 
 void turnOffLED(unsigned int gpio) {
-	setValue(gpio, 0);
-	setUnexport(gpio);
+        setUnexport(gpio);
+        setExport(gpio);
+setDirection(gpio, "out");
+    setValue(gpio, 0);
+    setUnexport(gpio);
 }
 
-int leds[4] = {396, 466, 392, 481};
+int leds[4] = {396, 392, 481, 398};
 
 void switchLED(string led, string status) {
-	int ledID = led[3] - '0';
-	for (int i = 0; i < 4; i++) {
+    int ledID = led[3] - '1';
+    for (int i = 0; i < 4; i++) {
         if (ledID == i) {
-	    	if (status == "on") {
-				turnOnLED(leds[i]);
-			} else {
-				turnOffLED(leds[i]);
-			}
-			return;
-		}
+            if (status == "on") {
+                turnOnLED(leds[i]);
+            } else {
+                turnOffLED(leds[i]);
+            }
+            return;
+        }
     }
 }
 
 void flashLED(int times) {
-	for(int i = 0; i < times; i++) {
-		turnOnLED(leds[0]);
-		turnOnLED(leds[1]);
-		turnOffLED(leds[2]);
-		turnOffLED(leds[3]);
-		this_thread::sleep_for(chrono::seconds(1));
-		turnOnLED(leds[3]);
-		turnOnLED(leds[2]);
-		turnOffLED(leds[1]);
-		turnOffLED(leds[0]);
-		this_thread::sleep_for(chrono::seconds(1));
-	}
+    for(int i = 0; i < times; i++) {
+        turnOnLED(leds[0]);
+        turnOnLED(leds[1]);
+        turnOffLED(leds[2]);
+        turnOffLED(leds[3]);
+        this_thread::sleep_for(chrono::seconds(1));
+        turnOnLED(leds[3]);
+        turnOnLED(leds[2]);
+        turnOffLED(leds[1]);
+        turnOffLED(leds[0]);
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    turnOffLED(leds[2]);
+        turnOffLED(leds[3]);
 }
 
 int main(int argc, char *argv[]) {
-	string firstArgument(argv[1]);
+    string firstArgument(argv[1]);
     string secondArgument(argv[2]);
 
+    cout << firstArgument << endl;
+    cout << secondArgument << endl;
     if(firstArgument.substr(0, 3) == "LED") {
-		switchLED(firstArgument, secondArgument);
-	} else if (firstArgument.substr(0, 10) == "Mode_Shine") {
-		int times = stoi(secondArgument);
-		flashLED(times);
-	} else {
-		perror("command not found");
-	}
+        switchLED(firstArgument, secondArgument);
+    } else if (firstArgument.substr(0, 10) == "Mode_Shine") {
+        int times = stoi(secondArgument);
+        flashLED(times);
+    } else {
+        perror("command not found");
+    }
 
     return 0;
 }
