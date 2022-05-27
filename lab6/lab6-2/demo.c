4ef6bd5c4eac046775a52fe299ccc2814511ab4c
justYu2001 <jack4781012@gmail.com>
2022-05-27 02:57:53 -0700

    fix gpio bug

--- lab6/lab6-2/demo.c
+++ lab6/lab6-2/demo.c
@@ -3,14 +3,15 @@
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <asm/uaccess.h>
+#include <stdio.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define DRIVER_MAJOR 60
 #define DRIVER_NAME "demo"
 
-int gpioPin[4] = { 396, 392, 481, 398 };
-int ledPin[4] = { 7, 12, 18, 29 };
+int gpioPin[4] = { 396, 255, 481, 254 };
+int ledPin[4] = { 7, 15, 18, 22 };
 int ledBook[4] = { 0, 0, 0, 0 };
 static char userChar[100];
 
@@ -42,15 +43,15 @@ static ssize_t driver_write(struct file *filp, const char *buf, size_t size, lof
         printk("userChar:%s\n", userChar);
 
         int ledIndex = userChar[size - 2] - '1';
+        int pin = ledPin[ledIndex];
 
         if(userChar[0] == 'g') {
             printk("LED%d Status: %d\n", ledIndex + 1, ledBook[ledIndex]);
         } else if (userChar[1] == 'n') {
-            gpio_direction_output(ledPin[ledIndex], 1);
+            gpio_set_value(pin, 1);
             ledBook[ledIndex] = 1;
         } else if (userChar[1] == 'f') {
-            gpio_set_value(ledPin[ledIndex], 0);
-            gpio_free(ledPin[ledIndex]);
+            gpio_set_value(pin, 0);
             ledBook[ledIndex] = 0;
         } else {
             printk("Error: command not found");
@@ -73,7 +74,23 @@ static int demo_init(void) {
     int result;
     printk("<1>demo: started\n");
 
-    /* Register character device */
+    char* ledName[4] = { "LED1", "LED2", "LED3", "LED4" };
+    
+    for (int i = 0; i < 4; i++) {
+        if (gpio_is_valid(gpioPin[i]) == 0) {
+            printk("pin %d is no valid\n", gpioPin[i]);
+            return -EBUSY;
+        }
+
+        if (gpio_request(gpioPin[i], ledName[i]) < 0) {
+            printk("pin %d is busy\n", gpioPin[i]);
+            return -EBUSY;
+        }
+
+        gpio_direction_output(gpioPinp[i], 0);
+        gpio_export(gpioPin[i], false);
+    }
+    
     result = register_chrdev(DRIVER_MAJOR, DRIVER_NAME, &driver_fops);
     if (result < 0) {
         printk("<1>demo: Failed to register character device\n");
@@ -85,6 +102,10 @@ static int demo_init(void) {
 
 static void demo_exit(void) {
     printk("<1>demo: removed\n");
+
+    for (int i = 0; i < 4; i++) {
+        gpio_free(gpioPin[i]);
+    }
 
     /* Unregister character device */
     unregister_chrdev(DRIVER_MAJOR, DRIVER_NAME);
