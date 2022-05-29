493fd6141ee0d3491eb1f25000649576ef9e30fb
justYu2001 <jack4781012@gmail.com>
2022-05-28 23:33:18 -0700

    fix pin error bug

--- lab6/lab6-2/demo.c
+++ lab6/lab6-2/demo.c
@@ -3,15 +3,14 @@
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <asm/uaccess.h>
-#include <stdio.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define DRIVER_MAJOR 60
 #define DRIVER_NAME "demo"
 
-int gpioPin[4] = { 396, 255, 481, 254 };
-int ledPin[4] = { 7, 15, 18, 22 };
+int gpioPin[4] = { 396, 392, 481, 388};
+int ledPin[4] = { 7, 12, 18, 37 };
 int ledBook[4] = { 0, 0, 0, 0 };
 static char userChar[100];
 
@@ -40,17 +39,19 @@ static ssize_t driver_write(struct file *filp, const char *buf, size_t size, lof
     printk("Write: Enter Write function\n");
     if(copy_from_user(userChar, buf, size) == 0){
         userChar[size - 1] = '\0';
-        printk("userChar:%s\n", userChar);
-
-        int ledIndex = userChar[4] - '1';
-        int pin = gpioPin[ledIndex];
-
+        int ledIndex;
+	    int pin;
         if(userChar[0] == 'g') {
+	        ledIndex = userChar[4] - '1';
             printk("LED%d Status: %d\n", ledIndex + 1, ledBook[ledIndex]);
         } else if (userChar[1] == 'n') {
+	    ledIndex = userChar[3] - '1';
+	        pin = gpioPin[ledIndex];
             gpio_set_value(pin, 1);
             ledBook[ledIndex] = 1;
         } else if (userChar[1] == 'f') {
+	        ledIndex = userChar[4] - '1';
+	        pin = gpioPin[ledIndex];
             gpio_set_value(pin, 0);
             ledBook[ledIndex] = 0;
         } else {
@@ -75,7 +76,6 @@ static int demo_init(void) {
     printk("<1>demo: started\n");
 
     char* ledName[4] = { "LED1", "LED2", "LED3", "LED4" };
-    
     int i;
     for (i = 0; i < 4; i++) {
         if (gpio_is_valid(gpioPin[i]) == 0) {
@@ -103,7 +103,6 @@ static int demo_init(void) {
 
 static void demo_exit(void) {
     printk("<1>demo: removed\n");
-
     int i;
     for (i = 0; i < 4; i++) {
         gpio_free(gpioPin[i]);
