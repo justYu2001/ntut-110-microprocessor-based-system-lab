99fd24fc6f841d35353f9c6db77527bb5cadca77
justYu2001 <jack4781012@gmail.com>
2022-06-23 00:28:14 +0800

    fix: turn off all leds after shot down backend

--- final/ws/app.py
+++ final/ws/app.py
@@ -1,5 +1,5 @@
 from xxlimited import new
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocketDisconnect
 from fastapi import FastAPI, WebSocket
 import asyncio
 import subprocess
@@ -12,34 +12,41 @@ websocket_app = FastAPI()
 async def getTime(websocket: WebSocket):
     await websocket.accept()
     timeString = "daytime"
-    while True:
-        await asyncio.sleep(1)
-        ADC_value = ADC_Sensor.get_value()
-        newTimeString = "night" if ADC_value > 800 else "daytime"
+    try:
+        while True:
+            await asyncio.sleep(1)
+            ADC_value = ADC_Sensor.get_value()
+            newTimeString = "night" if ADC_value > 800 else "daytime"
 
-        if newTimeString == "night" and settings.mode == "production":
-            subprocess.run("echo nvidia | sudo -S ./led_controller/multithread/controller on", shell = True)
-        
-        elif newTimeString == "daytime" and settings.mode == "production":
-            subprocess.run("echo nvidia | sudo -S ./led_controller/multithread/controller off", shell = True)
+            if newTimeString == "night" and settings.mode == "production":
+                subprocess.run("echo nvidia | sudo -S ./led_controller/multithread/controller on", shell = True)
+            
+            elif newTimeString == "daytime" and settings.mode == "production":
+                subprocess.run("echo nvidia | sudo -S ./led_controller/multithread/controller off", shell = True)
 
-        if timeString != newTimeString:
-            timeString = newTimeString
-            await websocket.send_text(timeString)
+            if timeString != newTimeString:
+                timeString = newTimeString
+                await websocket.send_text(timeString)
+    except WebSocketDisconnect:
+        subprocess.run("echo nvidia | sudo -S ./led_controller/multithread/controller off", shell = True)
 
 @websocket_app.websocket("/has_person")
 async def getTime(websocket: WebSocket):
     await websocket.accept()
 
-    while True:
-        await asyncio.sleep(1)
-        if camera.hasPerson():
-            if settings.mode == "production":
-                subprocess.run("./led_controller/driver/controller LED1 on", shell = True)
+    try:
+        while True:
+            await asyncio.sleep(1)
+            if camera.hasPerson():
+                if settings.mode == "production":
+                    subprocess.run("./led_controller/driver/controller LED1 on", shell = True)
 
-            await websocket.send_text("has person")
-        else:
-            if settings.mode == "production":
-                subprocess.run("./led_controller/driver/controller LED1 off", shell = True)
+                await websocket.send_text("has person")
+            else:
+                if settings.mode == "production":
+                    subprocess.run("./led_controller/driver/controller LED1 off", shell = True)
 
-            await websocket.send_text("no person")
+                await websocket.send_text("no person")
+    
+    except:
+        subprocess.run("./led_controller/driver/controller LED1 off", shell = True)
