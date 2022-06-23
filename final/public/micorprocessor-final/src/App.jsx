5add946aab3ab91764726b08c781112f3caf6f3f
justYu2001 <jack4781012@gmail.com>
2022-06-23 23:48:55 +0800

    fix: video width

--- final/public/micorprocessor-final/src/App.jsx
+++ final/public/micorprocessor-final/src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./components/Dashboard";
 const App = () => {
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-cyan-400 to-sky-500 flex justify-center items-center">
-            <div>
+            <div className="w-2/3 max-w-[1280px]">
                 <Video></Video>
                 <Dashboard></Dashboard>
             </div>
